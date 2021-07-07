6652849b7c6ae67e66793e3e9044ab55605a578f
zidol <ajh3166@naver.com>
2021-07-08 00:37:46 +0900

    컨트롤러 테스트

--- src/main/java/co/kr/nakdong/controller/HomeController.java
+++ src/main/java/co/kr/nakdong/controller/HomeController.java
@@ -1,13 +1,24 @@
 package co.kr.nakdong.controller;
 
+import co.kr.nakdong.entity.User;
+import co.kr.nakdong.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final UserService userService;
 
     @GetMapping("/index")
     public String index() {
@@ -19,5 +30,13 @@ public class HomeController {
     @GetMapping("/greeting")
     public String greeting() {
         return "hello";
+    }
+
+
+    @PreAuthorize("hasAnyRole('USER')")
+    @GetMapping("/user/{email}")
+    public ResponseEntity<User> test(@PathVariable String email) {
+        User user = userService.findByEmail(email).get();
+        return ResponseEntity.ok(user);
     }
 }
